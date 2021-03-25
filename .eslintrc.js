ec7433212536cda41cd38f2470f87813ccd4d279
ParkYoungWoong <thesecon@gmail.com>
2021-03-26 01:11:49 +0900

    Modify eslint config && Add comment

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,17 +1,21 @@
 module.exports = {
-  root: true,
+  // 사전에 정의된 전역 변수 설정
   env: {
+    browser: true,
     node: true
   },
+  // 사용 가능한 규칙 세트
   extends: [
     // 'plugin:vue/vue3-essential', // Lv1
     'plugin:vue/vue3-strongly-recommended', // Lv2
     // 'plugin:vue/vue3-recommended', // Lv3
     'eslint:recommended'
   ],
+  // 구문 분석할 패키지 설정(Babel, ES6^ => ES5)
   parserOptions: {
     parser: 'babel-eslint'
   },
+  // 추가적인 코드 규칙을 설정
   rules: {
     'vue/html-self-closing': ['error', {
       html: {
